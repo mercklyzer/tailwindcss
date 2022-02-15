10a752b4f512ac9b0878677f77b7ea4d19e86d19
mercklyzer <mercklyzer@gmail.com>
2022-02-15 23:46:53 +0800

    Add cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,11 @@ import React from "react"
 
 const Card = props => {
     return (
-        <div className="bg-white"> 
-            <img src={require('../assets/stew.jpg')} alt="stew" />
-            <div>
-                <span>5 Bean Chili Stew</span>
-                <span>Recipe by Mario</span>
+        <div className="bg-white rounded overflow-hidden shadow-md"> 
+            <img className="w-full h-32 sm:h-48 object-cover" src={require('../assets/stew.jpg')} alt="stew" />
+            <div className="m-4">
+                <span className="font-bold">5 Bean Chili Stew</span>
+                <span className="block text-gray-500 text-sm">Recipe by Mario</span>
             </div>
         </div>
     )
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,9 @@
 import React from "react"
+import Card from "./Card"
 
 const Content = props => {
     return (
-        <main className="px-16 py-6">
+        <main className="px-16 py-6 bg-gray-100">
             <div className="flex justify-end">
                 <a href="#" className="text-primary">Log in</a>
                 <a href="#" className="text-primary ml-2">Sign up</a>
@@ -18,7 +19,7 @@ const Content = props => {
         
                 <div className="mt-8">
                     {/* <!-- cards go here --> */}
-                    
+                    <Card />
                 </div>
 
                 <h4 className="font-bold mt-12 pb-2 border-b border-gray-200">Most Popular</h4>
