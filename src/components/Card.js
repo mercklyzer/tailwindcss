c769471bdf21923bbf103266098d146a82c8d847
mercklyzer <mercklyzer@gmail.com>
2022-02-16 10:30:41 +0800

    Add apply directive

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import React from "react"
 
-const Card = props => {
+const Card = ({menu, owner, image}) => {
     return (
-        <div className="bg-white rounded overflow-hidden shadow-md relative"> 
-            <img className="w-full h-32 sm:h-48 object-cover" src={require('../assets/stew.jpg')} alt="stew" />
+        <div className="card"> 
+            <img className="w-full h-32 sm:h-48 object-cover" src={image} alt="stew" />
             <div className="m-4">
-                <span className="font-bold">5 Bean Chili Stew</span>
-                <span className="block text-gray-500 text-sm">Recipe by Mario</span>
+                <span className="font-bold">{menu}</span>
+                <span className="block text-gray-500 text-sm">Recipe by {owner}</span>
             </div>
             <div className="bg-secondary-100 text-secondary-200 text-xs uppercase font-bold rounded-full p-2 absolute top-0 ml-2 mt-2">
                 <span>25 mins</span>
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,27 @@
 import React from "react"
 import Card from "./Card"
 
+const data = [
+    {
+        image: require('../assets/stew.jpg'),
+        menu: '5 Bean Chili Stew',
+        owner: 'Mario'
+    },
+    {
+        image: require('../assets/noodles.jpg'),
+        menu: 'Veg Noodles',
+        owner: 'Mario'
+    },
+    {
+        image: require('../assets/curry.jpg'),
+        menu: 'Tofu Curry',
+        owner: 'Mario'
+    },
+]
+
 const Content = props => {
     return (
-        <main className="px-16 py-6 bg-gray-100">
+        <main className="px-4 py-6 sm:px-16 bg-gray-100">
             <div className="flex justify-end">
                 <a href="#" className="text-primary">Log in</a>
                 <a href="#" className="text-primary ml-2">Sign up</a>
@@ -19,7 +37,9 @@ const Content = props => {
         
                 <div className="mt-8">
                     {/* <!-- cards go here --> */}
-                    <Card />
+                    {
+                        data.map(({menu, image, owner},i) => <Card key={i} menu={menu} image={image} owner={owner}/>)
+                    }
                 </div>
 
                 <h4 className="font-bold mt-12 pb-2 border-b border-gray-200">Most Popular</h4>
