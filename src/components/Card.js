a020adcba12e52ffe659b04459f8395d08027a8f
mercklyzer <mercklyzer@gmail.com>
2022-02-16 15:58:55 +0800

    Add hover

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ const Card = ({menu, owner, image}) => {
                 <span className="block text-gray-500 text-sm">Recipe by {owner}</span>
             </div>
             <div className="bg-secondary-100 text-secondary-200 text-xs uppercase font-bold rounded-full p-2 absolute top-0 ml-2 mt-2">
-                <svg class="w-5 h-5 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                <svg className="w-5 h-5 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                 <span>25 mins</span>
             </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Navbar = props => {
+    const [showMenu, setShowMenu] = useState(true)
+
     return(
-        <div className="md:col-span-1">
-            <nav>
-                <div>
+        <div className="md:col-span-1 md:flex md:justify-end">
+            <nav className="text-right">
+                <div className="flex justify-between items-center">
                     <h1 className="font-bold uppercase p-4 border-b border-gray-100">
                         <a href="/" className="hover:text-gray-700">Food Ninja</a>
                     </h1>
+                    <div className="px-4 cursor-pointer md:hidden" onClick={() => {
+                        setShowMenu((value) => !value)
+                        console.log("click")
+                    }}>
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                    </div>
                 </div>
-                <ul>
-                    <li className="text-gray-700 font-bold">
-                        <a href="#">
-                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
-                            <span>Home</span>
+                <ul className={`text-sm mt-6 ${showMenu?'hidden':''} md:block`}>
+                    <li className="text-gray-700 font-bold py-1 border-r-4 border-primary">
+                        <a href="#" className="px-4 flex justify-end">
+                            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+                            <span className="ml-2">Home</span>
                         </a>
                     </li>
-                    <li>
-                        <a href="#">
-                        <svg class="w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                            <span>About</span>
+                    <li className="py-1">
+                        <a href="#" className="px-4 flex justify-end">
+                        <svg className="w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            <span className="ml-2">About</span>
                         </a>
                     </li>
-                    <li>
-                        <a href="#">
-                            <svg class="w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
-                            <span>Contact</span>
+                    <li className="py-1">
+                        <a href="#" className="px-4 flex justify-end">
+                            <svg className="w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
+                            <span className="ml-2">Contact</span>
                         </a>
                     </li>
                 </ul>
